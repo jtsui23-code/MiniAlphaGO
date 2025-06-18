35f101e6c1e9f41edfbd8a15c394993af8f02c57
Jack Tsui <jacktsui23@gmail.com>
2025-06-18 01:29:51 -0500

    Documented all the evaulation components and did another model evaluation test.

--- training/evaluation.py
+++ training/evaluation.py
@@ -1,34 +1,74 @@
 from model.net import GoNet
 from training.selfPlay import modelTesting
+from training.train import createModel
 import torch
 
+
+# Creating the new candiateModel
+# The 7th save file was rejected i.e numTrainData=8
+
+createModel(numTrainData=8,fileName="candidateModel")
+
+# Loading the both the champion and candiate model to the network to be used in the 
+# evaulation.
 currentNetwork = GoNet(boardSize=9, channels=17)
 candidateModel = GoNet(boardSize=9, channels=17)
 
-currentNetwork.load_state_dict(torch.load("models/currentModel.pt"))
+# currentNetwork.load_state_dict(torch.load("models/currentModel.pt"))
+
+candidateModel.load_state_dict(torch.load("models/bestModel.pt"))
 candidateModel.load_state_dict(torch.load("models/candidateModel.pt"))
 
 currentNetwork.eval()
 candidateModel.eval()
 
-def evaulateModel(candiateModel, championModel, numGames=20):
 
+
+"""
+METHOD: evalateModel
+INPUT:
+    candiateModel (GoNet)      :  The model that is being checked if it might be the best.
+    championModel (GoNet)      :  The model that is the current best.
+
+RETURN:
+    N/A
+
+DESCRIPTION:
+    This function evaulates the candiate model to see if its maybe the new best model and if it is then 
+    the model is saved.
+    
+"""
+def evalateModel(candiateModel, championModel, numGames=20):
+
+    # Counter of all of the wins by the candiateModel.
     wins = 0
-
+    
+    # Have the 2 models compete a set number of games.
     for i in range(numGames):
-
+        
+        # Each model plays as black one after the other because playing as black gives the player/model an 
+        # inherit advantage.
         if i % 2 == 0:
+            
+            # The championModel gets the play as black this game.
             winner = modelTesting(blackModel=championModel, whiteModel=candiateModel) 
+
+            # Checking if the winner was the candiateModel if so then increment wins.
             if winner == -1:
                 wins += 1
 
-        elif i % 2 != 0:
+        else:
+            # The candiateModel gets the play as black this game.
             winner = modelTesting(blackModel=candiateModel, whiteModel=championModel)
+
+            # Checking if the winner was the candiateModel if so then increment wins.
             if winner == 1:
                 wins += 1
 
         print(f"Game {i+1}/{numGames} winner is {winner}") 
 
+    # Calculates the win rate of the candiateModel
+    # if the win rate is above 55% then the candiateModel is the new best model.
     winRate = (wins/numGames)
     print(f"Candiate model has a win rate of {winRate*100}%")
 
@@ -37,7 +77,7 @@ def evaulateModel(candiateModel, championModel, numGames=20):
     else:
         print("Candiate rejected")
 
-evaulateModel(candiateModel=candidateModel, championModel=currentNetwork)
+evalateModel(candiateModel=candidateModel, championModel=currentNetwork)
 
 
 
