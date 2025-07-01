87fbd33a0f6c489790f54efd758e1a56792d6f2d
Jack Tsui <jacktsui23@gmail.com>
2025-07-01 15:26:51 -0500

    Starting to work on frontend navigation bar.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,11 +4,13 @@ import Board
  from "@/components/board";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Daniel (The legendary Programmer) and Zack (The talented vibe coder) went hand in hand to create this powerful product.</h1>
+   
+    <div  className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+       <Nav></Nav> 
+      <h1>Daniel (The legendary Programmer) and Jack (The talented vibe coder) went hand in hand to create this powerful product.</h1>
       <p>It's blank now. let's get to work</p>
 
-      <Nav></Nav> 
+      
       <Board></Board>
     </div>
 
