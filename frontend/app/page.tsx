03c2932f87d956fcce3faf4903b07dc2a72de83d
csprincedaniel <csprincedaniel@gmail.com>
2025-07-01 14:48:32 -0500

    skeleton, and blank canvas

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
-
+import Nav from "@/components/nav";
+import Board
+ from "@/components/board";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>Daniel (The legendary Programmer) and Zack (The talented vibe coder) went hand in hand to create this powerful product.</h1>
       <p>It's blank now. let's get to work</p>
+
+      <Nav></Nav> 
+      <Board></Board>
     </div>
+
   );
 }
