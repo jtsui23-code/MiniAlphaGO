62252990569867f8d4aeb25db1e16d1f73e8e00e
Jack Tsui <jacktsui23@gmail.com>
2025-07-02 16:12:28 -0500

    Changed the positioning of the tabs on the navigation bar to be more streamline.

--- frontend/app/stats/page.tsx
+++ frontend/app/stats/page.tsx
@@ -4,14 +4,14 @@ import Board
  from "@/components/board";
 export default function Home() {
   return (
-   
+  <>
+    <Nav/> 
     <div  className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-       <Nav></Nav> 
-      stats
+      <p>It's blank now. let's get to work</p>
 
       
-      <Board></Board>
+      <Board/>
     </div>
-
+    </>
   );
 }
