8445f12a0fa697e1fc32a8fb95f0fb210f9e4cf9
Jack Tsui <jacktsui23@gmail.com>
2025-07-04 08:45:58 -0500

    Testing different coloring for the nav bar.

--- frontend/.next/server/chunks/ssr/_84d4f900._.js
+++ frontend/.next/server/chunks/ssr/_84d4f900._.js
@@ -28,8 +28,12 @@ __turbopack_context__.s({
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/server/route-modules/app-page/vendored/rsc/react-jsx-dev-runtime.js [app-rsc] (ecmascript)");
 ;
 function Nav() {
+    // Minit color nav bar
+    // return (<nav className="bg-gradient-to-r from-green-300 via-teal-300 to-emerald-400">
+    // Dark blue nav bar
+    // return (<nav className="bg-gradient-to-r from-gray-900 via-indigo-800 to-blue-700>
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("nav", {
-        className: "bg-gradient-to-r from-green-300 via-teal-300 to-emerald-400",
+        className: "bg-gradient-to-r from-orange-500 via-pink-500 to-red-500",
         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
             className: "flex justify-between items-center px-10 py-6 w-full text-black font-semibold text-lg",
             children: [
@@ -39,7 +43,7 @@ function Nav() {
                     children: " Home "
                 }, void 0, false, {
                     fileName: "[project]/components/nav.tsx",
-                    lineNumber: 7,
+                    lineNumber: 16,
                     columnNumber: 21
                 }, this),
                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
@@ -51,7 +55,7 @@ function Nav() {
                             children: " Play "
                         }, void 0, false, {
                             fileName: "[project]/components/nav.tsx",
-                            lineNumber: 12,
+                            lineNumber: 21,
                             columnNumber: 21
                         }, this),
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("a", {
@@ -60,7 +64,7 @@ function Nav() {
                             children: " Stats "
                         }, void 0, false, {
                             fileName: "[project]/components/nav.tsx",
-                            lineNumber: 13,
+                            lineNumber: 22,
                             columnNumber: 21
                         }, this),
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("a", {
@@ -69,24 +73,24 @@ function Nav() {
                             children: " Settings "
                         }, void 0, false, {
                             fileName: "[project]/components/nav.tsx",
-                            lineNumber: 14,
+                            lineNumber: 23,
                             columnNumber: 21
                         }, this)
                     ]
                 }, void 0, true, {
                     fileName: "[project]/components/nav.tsx",
-                    lineNumber: 9,
+                    lineNumber: 18,
                     columnNumber: 17
                 }, this)
             ]
         }, void 0, true, {
             fileName: "[project]/components/nav.tsx",
-            lineNumber: 6,
+            lineNumber: 15,
             columnNumber: 13
         }, this)
     }, void 0, false, {
         fileName: "[project]/components/nav.tsx",
-        lineNumber: 3,
+        lineNumber: 11,
         columnNumber: 13
     }, this);
 }
