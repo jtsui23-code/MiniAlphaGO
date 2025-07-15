6e41ef47aab4063ef19b6d810c456836b6b28179
Jack Tsui <jacktsui23@gmail.com>
2025-07-15 16:55:59 -0500

    Fixed bug with generateSymmetries.

--- utils/symmetries.py
+++ utils/symmetries.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 
 
@@ -22,29 +23,43 @@ def generateSymmetries(board, pi):
     boardSym = []
     piSym = []
 
-    piBoard = np.array(pi[:81]).reshape(9,9)
-    piPass = pi[81]
+    # boardNp = board.cpu().numpy()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Convert pi to tensor on GPU
+    # Need 2 variables because the 82th element in the list is pass which would not fit in a 
+    # 9x9 matrix.
+    piBoard = torch.tensor(pi[:81], device=device, dtype=torch.float32).reshape(9, 9)
+    piPass = torch.tensor([pi[81]], device=device, dtype=torch.float32)  # Keep as tensor
+
+    # piBoard = torch.tensor(pi[:81], device=device).reshape(9,9)
+
+    # piPass = pi[81]
 
     # Using k because k is a parameter in np.rot90 meaning how many 90% rotations to perform.
     for k in range(4):
 
-        # np.rot90() has 3 parameters (m, k, axes=()) 
+        # torch.rot90() has 3 parameters (m, k, dims=()) 
         # m - matrix
         # k - number of 90% rotations
         # axes - 2 axes which is rotated 
-        # (-2, -1) is used here because it uses the last 2 dimesnion as the axes of rotation. 
-        rotatedBoard = np.rot90(board, k, axes=(-2,-1))
-        rotatedPi = np.rot90(piBoard, k)
+        # (-2, -1) is used here because it uses the last 2 dimesnion as the dims of rotation. 
+        rotatedBoard = torch.rot90(board, k, dims=(-2, -1))
+        rotatedPi = torch.rot90(piBoard, k, dims=(-2, -1))
 
         boardSym.append(rotatedBoard)
 
-        # Have to .flatten() because earlier made the pi vector into a matrix
-        piSym.append(np.append(rotatedPi.flatten(), piPass))
 
-        flippedBoard = np.flip(rotatedBoard, axis=-1) # flips board horizontally
-        flippedPi = np.flip(rotatedPi, axis=1)
+        # Concatenates the pi tesnors as one on the GPU.
+        piSym.append(torch.cat([rotatedPi.flatten(), piPass]))
 
+        # Flips board and pi horizontally
+        flippedBoard = torch.flip(rotatedBoard, dims=[-1]) 
+        flippedPi = torch.flip(rotatedPi, dims=[-1])
+
+        
         boardSym.append(flippedBoard)
-        piSym.append(np.append(flippedPi.flatten(), piPass))
+        piSym.append(torch.cat([flippedPi.flatten(), piPass]))
 
     return boardSym, piSym
