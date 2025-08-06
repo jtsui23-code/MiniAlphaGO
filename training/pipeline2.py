eed852cbd16cf22b1a2336e44478c37e2d6e1a23
Jack Tsui <jacktsui23@gmail.com>
2025-08-06 18:03:04 -0500

    Ran pipeline

--- training/pipeline2.py
+++ training/pipeline2.py
@@ -255,8 +255,8 @@ while count < 2000:
         mct = 1000
         evalGames = 150
 
-    # if count == 0: 
-    #     numGames = 170
+    if count == 0: 
+        numGames = 150
 
 
     evalResult = startPipline(numGames=numGames, genNum=gen, mct=mct, evalGames=evalGames)
