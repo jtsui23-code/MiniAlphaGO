65beeb4e7169906b5d15171898e7b8504cb7dd42
Jack Tsui <jacktsui23@gmail.com>
2025-08-09 15:21:06 -0500

    Running pipeline

--- training/pipeline2.py
+++ training/pipeline2.py
@@ -256,7 +256,7 @@ while count < 2000:
         evalGames = 150
 
     if count == 0: 
-        numGames = 100
+        numGames = 270
 
 
     evalResult = startPipline(numGames=numGames, genNum=gen, mct=mct, evalGames=evalGames)
